ffdb965b214429292b9222a934ff72e1d2e74388
DadoLinux <ziocleto@gmail.com>
2020-05-04 16:35:31 +0100

    v1.0.2

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 class GlobalConfig {
-  constructor(lMongoDBUser, lMongoDBPass, lMongoDBURI, lMongoDBdbName, lMongoDBReplicaSetName, lJWTSecret, lSendGrid, lCloudHost, lTokenCookie, lAntiForgeryTokenCookie, lJwtExpiresSeconds, lReactHost, lFileRoot) {
+  constructor(lMongoDBUser, lMongoDBPass, lMongoDBURI, lMongoDBdbName, lMongoDBReplicaSetName, lJWTSecret, lSendGrid, lCloudHost, lTokenCookie, lAntiForgeryTokenCookie, lJwtExpiresSeconds, lReactHost, lFileRoot, lStelleStricePK, lStelleStriceSK) {
     this.MongoDBUser = lMongoDBUser;
     this.MongoDBPass = lMongoDBPass;
     this.MongoDBURI = lMongoDBURI;
@@ -13,10 +13,20 @@ class GlobalConfig {
     this.JwtExpiresSeconds = Number(lJwtExpiresSeconds);
     this.ReactHost = lReactHost
     this.fileRoot = lFileRoot
+    this.stelleStricePK = lStelleStricePK;
+    this.stelleStriceSK = lStelleStriceSK;
   }
 
   get FileRoot() {
     return this.fileRoot;
+  }
+
+  get StelleStricePK() {
+    return this.stelleStricePK;
+  }
+
+  get StelleStriceSK() {
+    return this.stelleStriceSK;
   }
 
   get JWTSecret() {
@@ -42,6 +52,8 @@ const gc = new GlobalConfig(
   , process.env.EH_JWT_EXPIRES_SECONDS
   , process.env.REACT_APP_EH_CLOUD_HOST
   , process.env.EH_FILE_ROOT
+  , process.env.EH_STELLE_E_STRISCE_PK
+  , process.env.EH_STELLE_E_STRISCE_SK
 );
 
 module.exports = gc;
